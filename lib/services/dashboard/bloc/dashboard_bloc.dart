4fad17fdbaed433c54c631fc5f6a06472f32bad1
vaishanvi <vaishanvi.ingole53@gmail.com>
2025-06-19 15:39:51 +0530

    second commit

--- lib/services/dashboard/bloc/dashboard_bloc.dart
+++ lib/services/dashboard/bloc/dashboard_bloc.dart
@@ -1,4 +1,3 @@
-import 'package:dispatch_management_system/services/dashboard/model/detail_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DashboardBloc extends Cubit<void> {
